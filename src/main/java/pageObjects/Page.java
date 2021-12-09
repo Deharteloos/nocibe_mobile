fc4b6206137bf07e9204d46a17f33b263d73c4f1
Deharteloos <bbebwanga@gmail.com>
2021-12-09 10:09:06 +0100

    Add some functions to Page class

--- src/main/java/pageObjects/Page.java
+++ src/main/java/pageObjects/Page.java
@@ -1,53 +1,98 @@
 package pageObjects;
 
+import config.ConfigPropertiesReader;
 import config.Properties;
+import config.SystemPropertiesReader;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidTouchAction;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class Page {
 
-    AppiumDriver driver;
-    public static final long WAIT = 10;
+    protected AppiumDriver driver;
+    protected AndroidTouchAction actions;
+
+    protected WebDriverWait shortWait;
+    protected WebDriverWait wait;
+    protected WebDriverWait longWait;
+    public static final long SHORT_WAIT = 2;
+    public static final long WAIT = 5;
+    public static final long LONG_WAIT = 10;
+
+    protected SystemPropertiesReader systemPropertiesReader;
+    protected ConfigPropertiesReader configPropertiesReader;
 
     public Page() {
         driver = Properties.APPIUM_DRIVER_MANAGER.getDriver();
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+        actions = new AndroidTouchAction(driver);
+        wait      = new WebDriverWait(driver, WAIT);
+        shortWait = new WebDriverWait(driver, SHORT_WAIT);
+        longWait  = new WebDriverWait(driver, LONG_WAIT);
+        systemPropertiesReader = Properties.SYSTEM_PROPERTIES_READER;
+        configPropertiesReader = Properties.CONFIG_PROPERTIES_READER;
     }
 
-    public void waitForVisibility(MobileElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, WAIT);
+    protected void waitForVisibility(MobileElement element) {
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    public void clear(MobileElement element) {
+    protected void get(String url) {
+        driver.get(url);
+    }
+
+    protected void clear(MobileElement element) {
         waitForVisibility(element);
         element.clear();
     }
 
-    public void click(MobileElement element) {
+    protected void click(MobileElement element) {
         waitForVisibility(element);
         element.click();
     }
 
-    public void sendTextSlowly(MobileElement element, String text) {
+    protected void sendTextSlowly(MobileElement element, String text) {
         waitForVisibility(element);
         for (int i = 0; i < text.length(); i++) {
             element.sendKeys(text.substring(i, i + 1));
         }
     }
 
-    public void sendText(MobileElement element, String text) {
+    protected void sendText(MobileElement element, String text) {
         waitForVisibility(element);
         element.sendKeys(text);
     }
 
-    public String getAttribute(MobileElement element, String attribute) {
+    protected String getAttribute(MobileElement element, String attribute) {
         waitForVisibility(element);
         return element.getAttribute(attribute);
+    }
+
+    protected void scrollDown() {
+        Dimension dimension = driver.manage().window().getSize();
+        actions.press(PointOption.point(0, (int) (dimension.getHeight() * 0.8)))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(3)))
+                .moveTo(PointOption.point(0, (int)((int) (dimension.getHeight() * 0.2))))
+                .release()
+                .perform();
+    }
+
+    protected void scrollUp() {
+        Dimension dimension = driver.manage().window().getSize();
+        actions.press(PointOption.point(0, (int) (dimension.getHeight() * 0.2)))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(3)))
+                .moveTo(PointOption.point(0, (int)((int) (dimension.getHeight() * 0.8))))
+                .release()
+                .perform();
     }
 
 }
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,29 +1,8 @@
 import config.Properties;
 import drivers.AppiumDriverManager;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.testng.annotations.*;
 
 public class TestRunner {
-
-    public static AppiumDriver driver;
-
-    public static void Android_setUp() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "9.0");
-        capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability("app",
-                System.getProperty("user.dir") + "/apks/ToDo_v1.24_apkpure.com.apk");
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
-    }
 
     @Parameters({"platform"})
     @BeforeClass
@@ -33,20 +12,9 @@ public class TestRunner {
                 .orElse(Properties.SYSTEM_PROPERTIES_READER.platformName));
     }
 
-    @Test
-    public void test() {
-        System.out.println(Properties.CONFIG_PROPERTIES_READER.appiumServer);
-        System.out.println(Properties.CONFIG_PROPERTIES_READER.app);
-        System.out.println(Properties.CONFIG_PROPERTIES_READER.appActivity);
-        System.out.println(Properties.CONFIG_PROPERTIES_READER.appPackage);
-        System.out.println(Properties.SYSTEM_PROPERTIES_READER.deviceName);
-        System.out.println(Properties.SYSTEM_PROPERTIES_READER.env.getUrl());
-    }
-
-    public static void tearDown() {
-        if(null != driver) {
-            driver.quit();
-        }
+    @AfterClass
+    public void tearDown() {
+        AppiumDriverManager.getDriver().quit();
     }
 
 }
